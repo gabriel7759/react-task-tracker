398058c61c59547de79dfb51c8faa2fbb64ab3cb
GVR <gabriel7759@hotmail.com>
2022-07-20 18:34:12 -0700

    Create a simple header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-function App() {
-  const name = 'Gabriel V.';
-  const x = true;
+import Header from "./components/Header";
 
+function App() {
   return (
     <div className="container">
-      <h1>Hello {name} From React</h1>
-      <h2>Hello {x ? 'Yes' : 'No'}</h2>
+      <Header />
     </div>
   );
 }
