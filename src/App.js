71afaea2ac722a79ae17f4e7a5ed76323ef15ccb
GVR <gabriel7759@hotmail.com>
2022-07-20 19:37:08 -0700

    Delete task sample

--- src/App.js
+++ src/App.js
@@ -22,12 +22,23 @@ function App() {
         day: 'Feb 5th at 2:30pm',
         reminder: false
     }
-]);
+  ]);
+
+  const deleteTask = (id) => {
+    console.log('delete ' + id);
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
 
   return (
     <div className="container">
       <Header />
-      <Tasks tasks={tasks} />
+      {tasks.length > 0 ? 
+      (
+        <Tasks tasks={tasks} onDelete={deleteTask} />
+      ) : (
+        "No tasks to show"
+      )}
+      
     </div>
   );
 }
